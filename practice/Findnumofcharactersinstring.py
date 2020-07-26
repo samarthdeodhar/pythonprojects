ea901e852c7118c39d3ed5b0e41c90c6f483c6af
samarthdeodhar <samarth.deodhar@gmail.com>
2020-07-26 12:17:31 -0700

    A fun Rock Paper Scissors game with the computer

---
+++ practice/Findnumofcharactersinstring.py
@@ -0,0 +1,18 @@
+sentence = "Hi, how are you! Hope your day goes bad!"
+print(sentence)
+ocr = 0
+#this  tells the computer that you have enter a letter so the computer will find how many times that letter appears.
+print ("Give me a character which you want me to find the occurrence of in the sentence above.")
+letter = str(input())
+#the for loop reads the sentence's characters and the iterator scans each character if it = the letter you entered.
+for i in range (len(sentence)):
+    if sentence[i] == letter:
+        ocr += 1
+print("your letter,",letter," appeared",ocr,"times")
+print("Done")
+
+
+
+
+
+
