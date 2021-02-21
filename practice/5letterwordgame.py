e3015c8d3790146d51e347b16c7a42b679c6508b
repl.it user <replituser@example.com>
2021-02-21 01:34:13 +0000

    Game of 5 letter words.

---
+++ practice/5letterwordgame.py
@@ -0,0 +1,19 @@
+words = []
+print("You play this game like this: Enter a five-letter word, and it will go in the list. If not a five-letter word, it will not. If you repeat a word, GAME OVER.")
+game = "play"
+
+while game == "play":
+
+    new = input("Give me a five-letter word.")
+    if len(new)> 5 or len(new) < 5:
+        print ("That's not a five-letter word.")
+    else:
+        if new in words:
+            game = "over"
+            print("Looks like you already entered that word. GAME OVER")
+            print("Your Score: You know", len(words), "five-letter words.")
+        else:
+            print("Nice one.")
+
+            words.append(new)
+
